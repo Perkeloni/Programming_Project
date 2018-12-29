f125ee0be94ef306c545d9f16729decb7bb97af5
Miguel Aço <miguelacopt@gmail.com>
2018-12-29 15:47:22 +0000

    Creating user is now working correctly, it writes into file, checks if it fulfills the criteria and warns the user if the username is already registered.

--- Project/Menus/Login_Checking_Utils.py
+++ Project/Menus/Login_Checking_Utils.py
@@ -40,22 +40,40 @@ def check_login(login, password):
 
 def create_account(login, password):
 
+    program_running = True
     root = Tk()
-    print(password)
+    duplicate = False
 
-    if re.match(r'^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,12}$', password):
-        file = open(r"C:\Users\User\PycharmProjects\Projecto_Programação\Project\Databases\Login_Database", "a")
-        input = login + " " + password + " 0" + "\n"
-        file.write(input)
+    while program_running:
+        file = open(r"C:\Users\User\PycharmProjects\Projecto_Programação\Project\Databases\Login_Database", "r")
+        line = file.readline()
+        print(line)
+        while line != "":
+            line = line.replace("/n", "")
+            line = line.split(" ")
+
+            if login == line[0]:
+                tkinter.messagebox.showinfo("Username In Use", "Your Username Is Already Registered, Please Try Another")
+                duplicate = True
+
+            line = file.readline()
+
+        program_running = False
         file.close()
-        tkinter.messagebox.showinfo("Account Created", "Your Account Has Been Created, Please Login Again")
-        root.destroy()
-        os.system("python Login_Menu.py")
 
-    else:
-        tkinter.messagebox.showinfo("Creating an Account", "Your password must contain, 6 letters, 1 uppercase, and a @ # or $")
-        print(password)
-        root.destroy()
-        os.system("python Login_Menu.py")
+        if re.match(r'^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,12}$', password) and duplicate == False:
+            file = open(r"C:\Users\User\PycharmProjects\Projecto_Programação\Project\Databases\Login_Database", "a")
+            input = login + " " + password + " 0" + "\n"
+            file.write(input)
+            file.close()
+            tkinter.messagebox.showinfo("Account Created", "Your Account Has Been Created, Please Login Again")
+            root.destroy()
+            os.system("python Login_Menu.py")
+
+        else:
+            tkinter.messagebox.showinfo("Creating an Account", "Your password must contain, 6 letters, 1 uppercase, and a @ # or $")
+            print(password)
+            root.destroy()
+            os.system("python Login_Menu.py")
 
 
